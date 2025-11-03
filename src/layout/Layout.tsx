adf5ff8e0a130d5c6cacd9d90052de0bf1e651b2
Fuza <tronglai301@gmail.com>
2025-11-03 14:47:56 +0700

    Css background,navbar web

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
-import Menu from "../commons/Menu";
+import Navbar from "../commons/NavBar";
 export default function Layout() {
-    return (
-    <div>
-        <Menu />
-        <Outlet />
-    </div> 
-    );
+  return (
+    <div className="w-full">
+      <Navbar />
+      <div className="w-full max-w-4/5 mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen relative">
+          <div className="fixed inset-0 -z-1 bg-partternCraft" />
+          <div className="">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
